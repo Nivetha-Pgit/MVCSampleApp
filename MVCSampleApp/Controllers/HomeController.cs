f4b55533551278573ed6fc2bec3597c821040cad
madhubalakrishnan <Madhu.Balakrishnan@cognizant.com>
2021-04-20 22:48:05 +0530

    first commit

---
+++ MVCSampleApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Versioning;
+using System.Web.Mvc;
+
+
+namespace MVCSampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var mvcName = typeof(Controller).Assembly.GetName();
+            var isMono = Type.GetType("Mono.Runtime") != null;
+
+            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+            //Store the values in Session
+            Session["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            Session["Runtime"] = isMono ? "Mono" : ".NET";
+
+            return View();
+        }
+    }
+}
